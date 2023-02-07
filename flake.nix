d34f3eee71495df3528ceb59541cefcbbfac5188
Fernando Ayats <ayatsfer@gmail.com>
2023-02-07 22:33:11 +0000

    add nixfiles

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    oxalica-rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["aarch64-linux" "x86_64-linux"];
+
+      perSystem = {
+        pkgs,
+        inputs',
+        ...
+      }: {
+        _module.args.pkgs = inputs'.nixpkgs.legacyPackages.extend inputs.oxalica-rust-overlay.overlays.default;
+
+        devShells.default = with pkgs; let
+          toolchain = rust-bin.fromRustupToolchainFile ./toolchain.toml;
+        in
+          mkShell {
+            RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
+            packages = [
+              toolchain
+            ];
+          };
+      };
+    };
+}
