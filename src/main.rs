579cbeffd43a8c493c667bd2aada776d12919122
Fernando Ayats <ayatsfer@gmail.com>
2023-02-15 14:46:37 +0100

    particles

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ use bevy_particle_systems::*;
 #[cfg(target_arch = "wasm32")]
 use web_sys::console;
 
+use bevy_web_asset::WebAssetPlugin;
+
 const BACKGROUND_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
 const TIME_STEP: f32 = 1.0 / 60.0;
 
@@ -25,7 +27,8 @@ fn main() {
     console_error_panic_hook::set_once();
 
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugin(WebAssetPlugin::default())
+        .add_plugins(DefaultPlugins.build().disable::<AssetPlugin>())
         .add_startup_system(setup)
         .add_plugin(ParticleSystemPlugin::default())
         .add_system_set(
@@ -44,19 +47,17 @@ fn setup(
 ) {
     commands.spawn(Camera2dBundle::default());
 
-    commands.spawn((
-        MaterialMesh2dBundle {
+    // commands.spawn((
+    // ));
+
+    commands
+        .spawn(StateVector(vec![-2.0, 0.0, 0.0, 1.0, 0.0]))
+        .insert(MaterialMesh2dBundle {
             mesh: meshes.add(shape::Circle::new(5.).into()).into(),
             material: materials.add(ColorMaterial::from(Color::PURPLE)),
             transform: Transform::from_translation(Vec3::new(-100., 0., 0.)),
             ..default()
-        },
-        StateVector(vec![-2.0, 0.0, 0.0, 1.0, 0.0]),
-    ));
-
-    commands.spawn((
-        StateVector(vec![-2.0, 0.0, 0.0, 1.0, 0.0]),
-        ParticleSystemBundle {
+        }).insert(ParticleSystemBundle {
             particle_system: ParticleSystem {
                 max_particles: 500,
                 emitter_shape: bevy_particle_systems::EmitterShape::CircleSegment {
@@ -64,9 +65,9 @@ fn setup(
                     direction_angle: std::f32::consts::PI,
                     radius: 0.0.into(),
                 },
-                texture: ParticleTexture::Sprite(asset_server.load("px.png")),
+                texture: ParticleTexture::Sprite(asset_server.load("https://media.githubusercontent.com/media/abnormalbrain/bevy_particle_systems/main/assets/px.png")),
                 spawn_rate_per_second: 35.0.into(),
-                initial_speed: JitteredValue::jittered(25.0, 0.0..0.0),
+                initial_speed: JitteredValue::jittered(25.0, 0.0..5.0),
                 acceleration: 0.0.into(),
                 lifetime: JitteredValue::jittered(3.0, -2.0..2.0),
                 color: ColorOverTime::Gradient(Gradient::new(vec![
@@ -80,26 +81,23 @@ fn setup(
                 rotation_speed: JitteredValue::jittered(0.0, -6.0..0.0),
                 ..ParticleSystem::default()
             },
+            // transform: Transform::from_xyz(-100.0, 50.0, 0.0),
+            transform: Transform::from_translation(Vec3::new(-100., 0., 0.)),
             ..ParticleSystemBundle::default()
-        },
-    ));
+        }).insert(Playing);
 }
+
+const A: [f32; 11] = [0.6, 10.0, 1.5, 1.5, 0.5, 3.8, 28.0, 0.2, 0.19, 0.9, 0.6];
 
 fn move_particle(mut query: Query<(&mut Transform, &mut StateVector)>) {
     for (mut transform, mut x) in &mut query {
-        // Transfer parameters
-        let a: [f32; 11] = [0.6, 10.0, 1.5, 1.5, 0.5, 3.8, 28.0, 0.2, 0.19, 0.9, 0.6];
-
         let g: [f32; 5] = [
-            a[0] * x[0] - a[1] * x[1] + a[2] * x[2] + a[3] * x[3] - a[10] * x[4],
-            a[4] * x[0] - a[5] * f32::atan(a[6] * x[1]),
-            -a[7] * x[0] - a[3] * x[3],
-            a[8] * x[2] - a[0] * x[4],
-            a[9] * x[4],
+            A[0] * x[0] - A[1] * x[1] + A[2] * x[2] + A[3] * x[3] - A[10] * x[4],
+            A[4] * x[0] - A[5] * f32::atan(A[6] * x[1]),
+            -A[7] * x[0] - A[3] * x[3],
+            A[8] * x[2] - A[0] * x[4],
+            A[9] * x[4],
         ];
-
-        let msg = format!("{:?}", g);
-        // console::log_1(&msg.into());
 
         for i in 0..4 {
             x[i] = x[i] + g[i] * TIME_STEP; // TODO dt
@@ -108,11 +106,4 @@ fn move_particle(mut query: Query<(&mut Transform, &mut StateVector)>) {
         transform.translation.x = x[0] * 30.0;
         transform.translation.y = x[1] * 30.0;
     }
-    // for (mut transform, velocity) in &mut query {
-    //     transform.translation.x += velocity.x * TIME_STEP;
-    //     transform.translation.y += velocity.y * TIME_STEP;
-
-    //     let msg = format!("{:?}", transform);
-    //     console::log_1(&msg.into());
-    // }
 }
