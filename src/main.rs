6c26dc5fed8dd2f9f21b3382aa4bebadaf48f819
Fernando Ayats <ayatsfer@gmail.com>
2023-02-15 11:00:22 +0000

    transfer testing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use bevy::math::vec3;
 use bevy::{prelude::*, sprite::MaterialMesh2dBundle, time::FixedTimestep};
+use bevy_particle_systems::*;
 use web_sys::console;
 
 const BACKGROUND_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
@@ -11,6 +12,9 @@ struct Particle;
 #[derive(Debug, Component, Deref, DerefMut)]
 struct Velocity(Vec2);
 
+#[derive(Debug, Component, Deref, DerefMut)]
+struct StateVector(Vec<f32>);
+
 fn main() {
     #[cfg(target_arch = "wasm32")]
     console_error_panic_hook::set_once();
@@ -18,6 +22,7 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
+        .add_plugin(ParticleSystemPlugin::default())
         .add_system_set(
             SystemSet::new()
                 .with_run_criteria(FixedTimestep::step(TIME_STEP as f64))
@@ -40,17 +45,40 @@ fn setup(
             transform: Transform::from_translation(Vec3::new(-100., 0., 0.)),
             ..default()
         },
-        Velocity(Vec2::new(10.0, 10.0)),
+        StateVector(vec![-2.0, 0.0, 0.0, 1.0, 0.0]),
     ));
+
+    // commands.spawn(ParticleSystemBundle {
+    //     particle_system: ParticleSystem {
+    //         max_particles: 10,
+    //         spawn_rate_per_second: 25.0.into(),
+    //         initial_speed: JitteredValue {
+    //             value: 0.0,
+    //             jitter_range: None,
+    //         },
+    //         lifetime: JitteredValue {
+    //             value: 8.0,
+    //             jitter_range: None,
+    //         },
+    //     },
+    //     ..ParticleSystemBundle::default()
+    // });
 }
 
-fn move_particle(mut query: Query<(&mut Transform, &Velocity)>) {
+fn move_particle(mut query: Query<(&mut Transform, &mut StateVector)>) {
+    for (mut transform, mut state_vector) in &mut query {
+        let transfer_params: [f64; 10] = [0.6, 10.0, 1.5, 0.5, 3.8, 28.0, 0.2, 0.19, 0.9, 0.6];
 
-    for (mut transform, velocity) in &mut query {
-        transform.translation.x += velocity.x * TIME_STEP;
-        transform.translation.y += velocity.y * TIME_STEP;
+        // let transfer = [
+        //     transfer_params[0]*state_vector[]
+        // ];
 
-        let msg = format!("{:?}", transform);
-        console::log_1(&msg.into());
     }
+    // for (mut transform, velocity) in &mut query {
+    //     transform.translation.x += velocity.x * TIME_STEP;
+    //     transform.translation.y += velocity.y * TIME_STEP;
+
+    //     let msg = format!("{:?}", transform);
+    //     console::log_1(&msg.into());
+    // }
 }
